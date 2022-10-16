87823030e84c4b44c0b18db99b16f35e11ee9455
Jakov Knezevic <knezevic.jakov@gmail.com>
2022-10-16 10:44:20 +0200

    Install more packages and restructure folders

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,7 @@
 module.exports = {
   mode: "jit",
   darkMode: false, // or 'media' or 'class'
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
