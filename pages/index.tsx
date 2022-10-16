85f35e7a1c70a0096659afde1fadcebffe7e0d84
Jakov Knezevic <knezevic.jakov@gmail.com>
2022-10-16 10:37:44 +0200

    Basic UI structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,17 @@ import Head from "next/head";
 import Image from "next/image";
 
 const Home: NextPage = () => {
-  return <div className="">Hello world</div>;
+  return (
+    <div className="h-screen w-screen flex flex-col justify-center items-center">
+      <div className="text-2xl text-center">Which Pokemon is rounder?</div>
+      <div className="p-2" />
+      <div className="border rounded p-8 flex justify-between items-center max-w-2xl">
+        <div className="w-16 h-16 bg-red-200"></div>
+        <div className="p-8">Vs</div>
+        <div className="w-16 h-16 bg-red-200"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
