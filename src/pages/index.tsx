79bcdea3e0eacaa73099624803db15fd4178c5e9
Jakov Knezevic <knezevic.jakov@gmail.com>
2022-10-16 18:38:16 +0200

    Styled buttons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,9 @@ import { useState } from "react";
 import { getOptionsForVote } from "@/utils/getRandomPokemon";
 import { trpc } from "@/utils/trpc";
 
+const btnStyle =
+  "inline-flex items-center px-2.5 py-1.5 border border-gray-300 shadow-sm text-xs font-medium rounded text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500";
+
 const Home = () => {
   const [ids, updateIds] = useState(() => getOptionsForVote());
 
@@ -13,12 +16,18 @@ const Home = () => {
 
   if (firstPokemon.isLoading || secondPokemon.isLoading) return null;
 
+  const voteForRoundest = (selected: number) => {
+    // TODO: fire mutation to persist changes
+
+    updateIds(getOptionsForVote());
+  };
+
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="text-2xl text-center">Which Pokemon is rounder?</div>
       <div className="p-2" />
       <div className="border rounded p-8 flex justify-between items-center max-w-2xl">
-        <div className="w-64 h-64 flex flex-col">
+        <div className="w-64 h-64 flex flex-col items-center">
           <img
             src={firstPokemon.data?.sprites.front_default || ""}
             className="w-full"
@@ -26,9 +35,12 @@ const Home = () => {
           <div className="text-xl text-center capitalize mt-[-2rem]">
             {firstPokemon.data?.name}
           </div>
+          <button className={btnStyle} onClick={() => voteForRoundest(first)}>
+            Rounder
+          </button>
         </div>
         <div className="p-8">Vs</div>
-        <div className="w-64 h-64 flex flex-col">
+        <div className="w-64 h-64 flex flex-col items-center">
           <img
             src={secondPokemon.data?.sprites.front_default || ""}
             className="w-full"
@@ -36,6 +48,9 @@ const Home = () => {
           <div className="text-xl text-center capitalize mt-[-2rem]">
             {secondPokemon.data?.name}
           </div>
+          <button className={btnStyle} onClick={() => voteForRoundest(second)}>
+            Rounder
+          </button>
         </div>
         <div className="p-2" />
       </div>
